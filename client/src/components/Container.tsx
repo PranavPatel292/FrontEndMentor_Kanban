d47902ee716946752d5a42c952f034bf1afdd9a8
Pranav Patel <pranav.patel292@gmail.com>
2023-03-19 20:06:42 +1100

    add form to create task

--- client/src/components/Container.tsx
+++ client/src/components/Container.tsx
@@ -1,8 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { ListsContainer } from "./ListContainer/ListsContainer";
 import { Header } from "./Header/Header";
-
-// client/tailwind.config.cjs
 
 export const Container = () => {
   return (

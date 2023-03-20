4886583740d48c4ec257b718dab9386e305e7792
Pranav Patel <pranav.patel292@gmail.com>
2023-03-20 12:23:01 +1100

    add prisma file

---
+++ server/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use("api/v1/board", require("./routes/board"));
+app.use("api/v1/column", require("./routes/column"));
+app.use("api/v1/task", require("./routes/task"));
+app.use("api/v1/subtask", require("./routes/subtask"));
+
+app.listen(port, () => console.log("Server listening on port " + port));
